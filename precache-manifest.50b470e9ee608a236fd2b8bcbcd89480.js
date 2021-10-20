b8e0718c0a807db9274f13c582c666ad6742cb29
unknown <alextq3036@gmail.com>
2021-10-20 14:48:06 -0400

    Updates

---
+++ precache-manifest.50b470e9ee608a236fd2b8bcbcd89480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74bcdbb7976e4faefaf53ac9888951f",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "4d75b29a4b82fde4dee2",
+    "url": "/random-quote-generator/static/css/main.056f24b6.chunk.css"
+  },
+  {
+    "revision": "8903fdcdbd77003951d8",
+    "url": "/random-quote-generator/static/js/2.9ecf2994.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-quote-generator/static/js/2.9ecf2994.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d75b29a4b82fde4dee2",
+    "url": "/random-quote-generator/static/js/main.d8b7e678.chunk.js"
+  },
+  {
+    "revision": "42d4644ddb0ad81f88c7",
+    "url": "/random-quote-generator/static/js/runtime-main.55678e65.js"
+  },
+  {
+    "revision": "240ca62de7e1ac88c3c9096ea4870114",
+    "url": "/random-quote-generator/static/media/background_forest.240ca62d.jpg"
+  }
+]);
